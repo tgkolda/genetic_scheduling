b4b3e048c0fd918122abf72baacade1ad2161e6a
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2022-11-15 14:47:34 -0500

    Process the organizers first names
    They were contained in a different part of the spreadsheet. There are
    some overlaps between last names, so a small amount of manual
    adjustments will be necessary.

--- data/SIAM-CSE23/csv2yaml.py
+++ data/SIAM-CSE23/csv2yaml.py
@@ -1,18 +1,34 @@
 import csv
 import yaml
 
+participants = []
+with open(r'//wsl.localhost/Ubuntu/home/amklinv/genetic_scheduling/data/SIAM-CSE23/participants.csv',encoding='utf8') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            line_count += 1
+        else:
+            # Only record the organizers
+            name = row[0]
+            role = row[1]
+            
+            if role == "ORG: Mini organizer":
+                participants.append(name)
+            line_count += 1
+    participants = set(participants)
+
 minisymposia = {}
 with open(r'//wsl.localhost/Ubuntu/home/amklinv/genetic_scheduling/data/SIAM-CSE23/minisymposia.csv',encoding='utf8') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
             codes = row[0:3]
             title = row[4]
-            organizers = row[5]
+            organizers = row[5].split(',')
             sessions = row[6:11]
 
             speakers = []
@@ -25,12 +41,25 @@ with open(r'//wsl.localhost/Ubuntu/home/amklinv/genetic_scheduling/data/SIAM-CSE
                 talks.append(words[2])
 
             minisymposia[title] = {}
-            minisymposia[title]["organizer"] = organizers
+            minisymposia[title]["organizers"] = []
+            for organizer in organizers:
+                organizer = organizer.strip()
+                numhits = 0
+                for participant in participants:
+                    if participant.startswith(organizer + " "):
+                        strlen = len(organizer)
+                        solution = participant[strlen+1:] + " " + organizer
+                        numhits+=1
+                if numhits != 1:
+                    print("There are too many hits for", organizer)
+                    minisymposia[title]["organizers"].append(organizer)
+                else:
+                    minisymposia[title]["organizers"].append(solution)     
+
             minisymposia[title]["speakers"] = speakers
             minisymposia[title]["talks"] = talks
             minisymposia[title]["class codes"] = list(map(int, codes))
             line_count += 1
-    print(f'Processed {line_count} lines.')
 
 with open(r'//wsl.localhost/Ubuntu/home/amklinv/genetic_scheduling/data/SIAM-CSE23/minisymposia.yaml', 'w') as file:
     yaml.dump(minisymposia, file)
@@ -41,7 +70,6 @@ with open(r'//wsl.localhost/Ubuntu/home/amklinv/genetic_scheduling/data/SIAM-CSE
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
             title = row[8]
@@ -52,7 +80,6 @@ with open(r'//wsl.localhost/Ubuntu/home/amklinv/genetic_scheduling/data/SIAM-CSE
             lectures[title]["speaker"] = speaker
             lectures[title]["class codes"] = list(map(int, codes))
             line_count += 1
-    print(f'Processed {line_count} lines.')
 
 with open(r'//wsl.localhost/Ubuntu/home/amklinv/genetic_scheduling/data/SIAM-CSE23/lectures.yaml', 'w') as file:
     yaml.dump(lectures, file)
