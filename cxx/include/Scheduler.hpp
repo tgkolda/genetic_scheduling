b93ebdcdd8bc61998af319b55283f671cefe4a8e
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2022-11-21 16:27:29 -0500

    Fix schedule issues when possible
    If there are problems detected in the schedule that are easy to fix,
    like two minisymposia being in the wrong order, or a more (predicted)
    popular talk being put in a smaller room than a less popular talk, fix
    them during the rating phase of the code.

--- cxx/include/Scheduler.hpp
+++ cxx/include/Scheduler.hpp
@@ -4,6 +4,7 @@
 #include "Minisymposia.hpp"
 #include "Rooms.hpp"
 #include "Schedule.hpp"
+#include "Utility.hpp"
 #include "Kokkos_Random.hpp"
 #include <fstream>
 #include <random>
@@ -21,10 +22,12 @@ public:
   KOKKOS_INLINE_FUNCTION double rate(View2D schedule) const;
 
   template<class View2D>
+  KOKKOS_INLINE_FUNCTION void fix_order(View2D schedule) const;
+
+  template<class View2D>
   inline void record(const std::string& filename, View2D schedule) const;
 
   KOKKOS_FUNCTION bool out_of_bounds(unsigned i) const;
-  void fix_schedules();
   KOKKOS_FUNCTION unsigned nslots() const;
   KOKKOS_FUNCTION unsigned nrooms() const;
   void record(const std::string& filename) const;
@@ -37,11 +40,49 @@ private:
 
 template<class View2D>
 double Scheduler::rate(View2D schedule) const {
+  fix_order(schedule);
+
   unsigned order_penalty, oversubscribed_penalty, priority_penalty;
   double theme_penalty;
   double result = mini_.rate_schedule(schedule, order_penalty, oversubscribed_penalty, 
                                       theme_penalty, priority_penalty);
   return result;
+}
+
+template<class View2D>
+void Scheduler::fix_order(View2D schedule) const {
+  unsigned nmini = mini_.size();
+
+  // If we can put multi-part minisymposia in order, do that
+  for(unsigned sl1=0; sl1<nslots(); sl1++) {
+    for(unsigned r1=0; r1<nrooms(); r1++) {
+      if(schedule(sl1,r1) >= nmini) continue;
+      for(unsigned sl2=sl1+1; sl2<nslots(); sl2++) {
+        for(unsigned r2=0; r2<nrooms(); r2++) {
+          if(schedule(sl2,r2) >= nmini) continue;
+          if(mini_.breaks_ordering(schedule(sl1,r1), schedule(sl2,r2))) {
+            // swap the values
+            genetic::swap(schedule(sl1, r1), schedule(sl2, r2));
+          }
+        }
+      }
+    }
+  }
+
+  // Sort the minisymposia in each slot based on the room priority
+  for(unsigned sl=0; sl<nslots(); sl++) {
+    for(unsigned i=1; i<nrooms(); i++) {
+      for(unsigned j=0; j<nrooms()-i; j++) {
+        auto m1 = schedule(sl,j);
+        auto m2 = schedule(sl,j+1);
+        if(m2 >= nmini) continue;
+        if(m1 >= nmini || mini_[m2].priority() < mini_[m1].priority()) {
+          // swap the values
+          genetic::swap(schedule(sl, j), schedule(sl, j+1));
+        }
+      }
+    }
+  }
 }
 
 template<class View2D>
--- cxx/src/Scheduler.cpp
+++ cxx/src/Scheduler.cpp
@@ -21,48 +21,6 @@ bool Scheduler::out_of_bounds(unsigned i) const {
   return i >= mini_.size();
 }
 
-void Scheduler::fix_schedules() {
-/*
-  unsigned nmini = mini_.size();
-  Kokkos::parallel_for("fix schedules", nschedules(), KOKKOS_CLASS_LAMBDA(unsigned sc) {
-    // If we can put multi-part minisymposia in order, do that
-    for(unsigned sl1=0; sl1<nslots(); sl1++) {
-      for(unsigned r1=0; r1<nrooms(); r1++) {
-        if(current_schedules_(sc,sl1,r1) >= nmini) continue;
-        for(unsigned sl2=sl1+1; sl2<nslots(); sl2++) {
-          for(unsigned r2=0; r2<nrooms(); r2++) {
-            if(current_schedules_(sc,sl2,r2) >= nmini) continue;
-            if(mini_.breaks_ordering(current_schedules_(sc,sl1,r1), current_schedules_(sc,sl2,r2))) {
-              // swap the values
-              auto temp = current_schedules_(sc,sl1,r1);
-              current_schedules_(sc,sl1,r1) = current_schedules_(sc,sl2,r2);
-              current_schedules_(sc,sl2,r2) = temp;
-            }
-          }
-        }
-      }
-    }
-
-    // Sort the minisymposia in each slot based on the room priority
-    for(unsigned sl=0; sl<nslots(); sl++) {
-      for(unsigned i=1; i<nrooms(); i++) {
-        for(unsigned j=0; j<nrooms()-i; j++) {
-          auto m1 = current_schedules_(sc,sl,j);
-          auto m2 = current_schedules_(sc,sl,j+1);
-          if(m2 >= nmini) continue;
-          if(m1 >= nmini || mini_[m2].higher_priority(mini_[m1])) {
-              // swap the values
-              auto temp = current_schedules_(sc,sl,j);
-              current_schedules_(sc,sl,j) = current_schedules_(sc,sl,j+1);
-              current_schedules_(sc,sl,j+1) = temp;
-          }
-        }
-      }
-    }
-  });
-*/
-}
-
 KOKKOS_FUNCTION
 unsigned Scheduler::nslots() const {
   return ntimeslots_;
