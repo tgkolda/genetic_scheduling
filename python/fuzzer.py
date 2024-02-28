11d4eca9267d9db566912ea9f0070da0099a29e6
Alicia Klinvex <alicia.klinvex@unnpp.gov>
2024-02-28 15:13:58 -0500

    Entity resolution now uses nicknames
    The goal is to detect whether Mike Heroux and
    Michael Heroux are the same person.

--- python/fuzzer.py
+++ python/fuzzer.py
@@ -1,6 +1,31 @@
 from thefuzz import fuzz
+from nicknames import NickNamer
 import yaml
 import numpy as np
+
+# Using nicknames is meant to match names like
+# Chris Vogl and
+# Christopher Vogl
+# with a 100% similarity score.
+# Unfortunately, this predominantly captures American nicknames,
+# but there's no reason we can't improve the nickname database.
+nn = NickNamer()
+
+def get_nicknames(name):
+    # Grab the first name to determine nicknames
+    separated = name.split()
+    first_name = separated[0]
+
+    # Get a list of nicknames/canonical names
+    first_names = nn.nicknames_of(separated[0]) | nn.canonicals_of(separated[0])
+    first_names.add(first_name)
+
+    # Add back the surnames
+    full_names = []
+    for first_name in first_names:
+        full_name = ' '.join([first_name] + separated[1:])
+        full_names.append(full_name)
+    return full_names
 
 # Read the minisymposia
 with open(r'data/SIAM-CSE23/minisymposia.yaml') as file:
@@ -24,30 +49,34 @@ name_pairs = []
 scores = []
 n = len(names)
 for i in range(n):
-    name1 = names[i]
+    names1 = get_nicknames(names[i])
+    # Compare against other names
     for j in range(i+1, n):
-        name2 = names[j]
+        names2 = get_nicknames(names[j])
+        best_ratio = 0
+        for name1 in names1:
+            for name2 in names2:
+                # This score will be high for simple misspellings
+                # For example,
+                # Sri Hari Krishna Narayanan and
+                # Sri Hari Krishn Narayanan
+                # score a 98% similarity
+                # Bart Van Bloemen Waanders and
+                # Bart van Bloeme Waanders
+                # score a 94% similarity
+                ratio = fuzz.ratio(name1, name2)
 
-        # This score will be high for simple misspellings
-        # For example,
-        # Sri Hari Krishna Narayanan and
-        # Sri Hari Krishn Narayanan
-        # score a 98% similarity
-        # Bart Van Bloemen Waanders and
-        # Bart van Bloeme Waanders
-        # score a 94% similarity
-        ratio = fuzz.ratio(name1, name2)
+                # This score accounts for middle names/initials
+                # For example,
+                # Patrick Farrell and
+                # Patrick E. Farrell
+                # score a 100% similarity
+                tsratio = fuzz.token_set_ratio(name1, name2)
 
-        # This score accounts for middle names/initials
-        # For example,
-        # Patrick Farrell and
-        # Patrick E. Farrell
-        # score a 100% similarity
-        tsratio = fuzz.token_set_ratio(name1, name2)
+                best_ratio = max(ratio, tsratio, best_ratio)
 
-        best_ratio = max(ratio, tsratio)
         if best_ratio > 80:
-            name_pairs.append(f'{name1}\n{name2}')
+            name_pairs.append(f'{names[i]}\n{names[j]}')
             scores.append(best_ratio)
 
 npscores = np.array(scores)
